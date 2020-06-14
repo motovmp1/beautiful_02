bdb1871fc72f10b9d8623a561a31a2209784fe99
motovmp1 <vinicius.mpinho@gmail.com>
2020-06-14 10:31:13 +0800

    Running 2

--- beausopa_005.py
+++ beausopa_005.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as beau
 
 soup = beau(open('/home/elsys/PycharmProjects/beautiful_01/bares.html'), "html.parser")
 
-filename = "bares.csv"
+filename = "bares3.csv"
 f = open(filename, "w")
 
 # headers = "ENDERECO, NUMERO, BARES\n"
@@ -19,12 +19,19 @@ f = open(filename, "w")
 # for title in soup.findAll('div', {"class": "jet-listing-dynamic-field__content"}):
 
 
-nome_bar = soup.findAll('div', {"class": "elementor elementor-61"})
+nome_bar = soup.findAll('div', {"class": "elementor-widget-container"})
 # print(len(nome_bar))
 # print(nome_bar[3].text)
 
-print(soup.prettify(nome_bar[0]))
 
+filename = "bares3.csv"
+f = open(filename, "w")
 
-# f.close()
+headers = "ENDERECO, NUMERO, BARES\n"
+f.write(headers)
 
+for list in nome_bar:
+    print(list.text.strip())
+    f.write(list.text.replace(",", "") + "," + "\n")
+
+f.close()
